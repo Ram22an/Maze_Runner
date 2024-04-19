f5341eceeffe3b1da89d8ea63bf643edbc01b3ca
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-04-19 22:56:44 +0530

    Adding Player Animation

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,59 +5,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float moveSpeed = 5f;
+    private float moveForward, rotateInput;
+    private float moveSpeed = 25f;
     private float turnSpeed = 100f;
-    //private float rotY;
-    //private float TurnSpeed=1f;
-    //private float moveForward, RotateLeftorRight;
-    private CharacterController characterController;
-    //private Rigidbody myBody;
-    public float Speed = 10f;
+    private CharacterController characterController;    
+    private Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
-        //myBody=GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
+        animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         PlayerMove();
     }
+    void FixedUpdate()
+    {
+        PlayerAnimation();
+    }
+
+    void PlayerAnimation()
+    {
+        
+        if (moveForward != 0)
+        {
+            animator.ResetTrigger("Stop");
+            animator.SetTrigger("Run");
+        }
+        else
+        {
+            animator.ResetTrigger("Run");
+            animator.SetTrigger("Stop");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            AnimatorClipInfo[] clip = animator.GetCurrentAnimatorClipInfo(0);
+            if (clip.Length > 0)
+            {
+                if (clip[0].clip.name == "run")
+                {
+                    animator.SetTrigger("RunAttack");
+                }
+                else if (clip[0].clip.name == "idle")
+                {
+                    animator.SetTrigger("Attack");
+                }
+            }
+        }
+        if (!characterController.isGrounded)
+        {
+            AnimatorClipInfo[] clip = animator.GetCurrentAnimatorClipInfo(0);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (clip.Length > 0)
+                {
+                    if (clip[0].clip.name == "run")
+                    {
+                        animator.SetTrigger("Jump");
+                    }
+                    else if (clip[0].clip.name == "idle")
+                    {
+                        animator.SetTrigger("Jump");
+                    }
+                }
+            }
+        }
+    }
+
+
+
 
     void PlayerMove()
     {
-        float moveForward = Input.GetAxisRaw("Vertical");
-        float rotateInput = Input.GetAxisRaw("Horizontal");
-
+        moveForward = Input.GetAxisRaw("Vertical");
+        rotateInput = Input.GetAxisRaw("Horizontal");
         Vector3 moveDirection = transform.forward * moveForward;
         characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
-
         transform.Rotate(Vector3.up, rotateInput * turnSpeed * Time.deltaTime);
-        //RotateLeftorRight = 0;
-        /***moveForward = 0;
-        if (Input.GetAxisRaw("Vertical")>0)
-        {
-            moveForward = 1;
-        }
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            moveForward = -1;
-        }
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            RotateLeftorRight = 1;
-        }
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            RotateLeftorRight = -1;
-            
-        }
-        //rotY += RotateLeftorRight * TurnSpeed;
-        //characterController.transform.Rotate(new Vector3(0f,rotY,0f));
-        characterController.Move(new Vector3(0f, 0f, Speed * moveForward*Time.deltaTime));***/
-
     }
 
 
@@ -66,3 +91,30 @@ public class PlayerMovement : MonoBehaviour
 
 
 }//class
+ //public float Speed = 50f;
+ //private float rotY;
+ //private float TurnSpeed=1f;
+ //private float moveForward, RotateLeftorRight;
+ //private Rigidbody myBody;
+ //RotateLeftorRight = 0;
+/***moveForward = 0;
+if (Input.GetAxisRaw("Vertical")>0)
+{
+    moveForward = 1;
+}
+else if (Input.GetAxisRaw("Vertical") < 0)
+{
+    moveForward = -1;
+}
+if (Input.GetAxisRaw("Horizontal") > 0)
+{
+    RotateLeftorRight = 1;
+}
+if (Input.GetAxisRaw("Horizontal") < 0)
+{
+    RotateLeftorRight = -1;
+
+}
+//rotY += RotateLeftorRight * TurnSpeed;
+//characterController.transform.Rotate(new Vector3(0f,rotY,0f));
+characterController.Move(new Vector3(0f, 0f, Speed * moveForward*Time.deltaTime));***/
